c2634e350098f8dcc22032c6b2034f85318f39b0
Dean Hamstead <dean@fragfest.com.au>
2023-07-10 12:26:35 -0700

    Add more OS support for system perls

---
+++ lib/PerlDevEnv/Perl/Perlbrew/Module.pm
@@ -0,0 +1,34 @@
+#!/bin/false
+# vim: softtabstop=4 tabstop=4 shiftwidth=4 ft=perl expandtab smarttab
+
+package PerlDevEnv::Perl::Perlbrew;
+
+use Rex -base;
+
+task install_perlbrew => sub {
+
+    my @packages;
+
+    push @packages, ('perlbrew', 'perl-devel-patchperl')
+        if is_arch;
+    push @packages, 'p5-App-perlbrew'
+        if is_freebsd;
+    push @packages, ('perl-App-perlbrew', 'perl-Devel-PatchPerl')
+        if is_suse;
+
+    die "Unable to install Perlbrew for this OS\n"
+        unless @packages;
+
+    pkg \@packages,
+        ensure => 'latest';
+
+};
+
+task prepare => sub {
+
+    Rex::Logger::info(__PACKAGE__);
+
+    install_perlbrew();
+};
+
+1;
