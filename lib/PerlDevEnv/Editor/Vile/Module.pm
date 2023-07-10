66e8d43d6dc890d9f7d13cfeffacca5ab29e4870
Dean Hamstead <dean@fragfest.com.au>
2023-07-10 12:27:00 -0700

    Add support for the Vile editor

---
+++ lib/PerlDevEnv/Editor/Vile/Module.pm
@@ -0,0 +1,25 @@
+#!/bin/false
+# vim: softtabstop=4 tabstop=4 shiftwidth=4 ft=perl expandtab smarttab
+
+package PerlDevEnv::Editor::Vile;
+
+use Rex -base;
+use Rex::Commands::SCM;
+
+task prepare => sub {
+
+    Rex::Logger::info(__PACKAGE__);
+
+    my @packages;
+
+    push @packages, qw/ vile /
+        if is_arch or is_debian or is_freebsd or is_suse or is_redhat;
+
+    die "Unable to configure Vile for this OS\n"
+        unless @packages;
+
+    pkg \@packages, ensure => 'latest';
+
+};
+
+1;
