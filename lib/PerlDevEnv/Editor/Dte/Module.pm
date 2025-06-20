9fe196457c06c38f6375b0261e9ffbcd12685258
Dean Hamstead <dean@fragfest.com.au>
2025-06-20 18:24:46 +1000

    Add Dte editor

---
+++ lib/PerlDevEnv/Editor/Dte/Module.pm
@@ -0,0 +1,25 @@
+#!/bin/false
+# vim: softtabstop=4 tabstop=4 shiftwidth=4 ft=perl expandtab smarttab
+
+package PerlDevEnv::Editor::Dte;
+
+use Rex -base;
+
+task prepare => sub {
+
+    Rex::Logger::info(__PACKAGE__);
+
+    my @packages;
+
+    if ( is_arch or is_debian or is_freebsd or is_suse or is_redhat ) {
+        push @packages, qw/ dte /;
+    }
+
+    die "Unable to configure Dte for this OS\n"
+      unless @packages;
+
+    pkg \@packages, ensure => 'latest';
+
+};
+
+1;
